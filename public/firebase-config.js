62796d1f1760047d0deb1c247e1c6f784a8fb544
Po Tsui <potsui@stanford.edu>
2017-09-10 07:14:02 -0400

    Move private config out of public

---
+++ public/firebase-config.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyAAfTBK-hBUOG7t90LmQdnH8ZcvXBBQkOg",
+    authDomain: "medhacks2017.firebaseapp.com",
+    databaseURL: "https://medhacks2017.firebaseio.com",
+    projectId: "medhacks2017",
+    storageBucket: "medhacks2017.appspot.com",
+    messagingSenderId: "557158205606"
+};
